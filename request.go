5f0a0891c64b02bfb18dec452bc86b4b5f196010
ma6174 <ma617495@gmail.com>
2021-01-24 22:09:11 +0800

    inc buffer

--- request.go
+++ request.go
@@ -362,7 +362,7 @@ type closeWriter interface {
 // proxy is used to suffle data from src to destination, and sends errors
 // down a dedicated channel
 func proxy(dst io.Writer, src io.Reader, errCh chan error) {
-	_, err := io.Copy(dst, src)
+	_, err := io.CopyBuffer(dst, src, make([]byte, 128*1024))
 	if tcpConn, ok := dst.(closeWriter); ok {
 		tcpConn.CloseWrite()
 	} else if c, ok := dst.(io.Closer); ok {
